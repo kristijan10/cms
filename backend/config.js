296a88f5af69a10c477070a95fd6df275cd9cbc9
kristijan <kristijan@kristijans-MacBook-Pro.local>
2025-04-01 17:14:10 +0200

    uklonjen promise iz mysql zbog kresovanja deploya

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-import mysql from "mysql2/promise";
+import mysql from "mysql2";
 
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 // const local = {
