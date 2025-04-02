4adc17563a3e68e6643d431253952417f0fabfef
kristijan <kristijan@kristijans-MacBook-Pro.local>
2025-04-02 14:45:56 +0200

    izmena zbog prikaza unutar railway dashboard

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,13 @@
 import mysql from "mysql2/promise";
 
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+const url = process.env.MYSQL_URL;
 const local = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
 };
-const connection = await mysql.createConnection(urlDB);
+const connection = await mysql.createConnection(url);
 
 export default connection;
