66cf2d598350a14269ed8e64c7d3a399827de0e4
kristijan <kristijan@kristijans-MacBook-Pro.local>
2025-04-03 10:34:30 +0200

    izmenjen cors origin

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://www.postman.com", "http://localhost:3000"],
+    // origin: ["https://www.postman.com", "http://localhost:3000"],
+    origin: "*",
     methods: ["GET", "PUT", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
